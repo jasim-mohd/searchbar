027f9800fb6eda5484febad909b14b9b2416dfd6
user <user@x230.local>
2017-06-18 02:55:32 +0400

    Initial Commit

---
+++ searchbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchbar
+//
+//  Created by user on 6/18/17.
+//  Copyright © 2017 Mohamed Jasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searchbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  searchbar
+//
+//  Created by user on 6/18/17.
+//  Copyright © 2017 Mohamed Jasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
